badec687a91b811eeb254f1d1a34ea9e1e000fea
jqyang3108 <jqyang3108@gmail.com>
2018-09-17 15:43:20 -0400

    Homework5 --updated 09-17-2018

---
+++ hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/Base.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/Base.cpp.o"
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/PrivDerived.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/PrivDerived.cpp.o"
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/ProtDerived.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/ProtDerived.cpp.o"
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/PublicDerived.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/PublicDerived.cpp.o"
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/dPriv.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/dPriv.cpp.o"
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/dProt.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/dProt.cpp.o"
+  "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/dPublic.cpp" "/Users/jiaqiyang/Files/Academic/Fall2018/ECE 30862/CPP-Practices/hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/dPublic.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw5/HW5a/cmake-build-debug/CMakeFiles/main.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.cpp.dir/Base.cpp.o"
+  "CMakeFiles/main.cpp.dir/dPublic.cpp.o"
+  "CMakeFiles/main.cpp.dir/dProt.cpp.o"
+  "CMakeFiles/main.cpp.dir/dPriv.cpp.o"
+  "CMakeFiles/main.cpp.dir/PublicDerived.cpp.o"
+  "CMakeFiles/main.cpp.dir/ProtDerived.cpp.o"
+  "CMakeFiles/main.cpp.dir/PrivDerived.cpp.o"
+  "main.cpp.pdb"
+  "main.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
